a1c582c89687ecceab0417ad6073d5421aeaf855
aldo <rojovivo_27@hotmail.com>
2018-09-04 19:50:01 -0500

    Initial Commit

---
+++ Practica3-TableViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practica3-TableViewController
+//
+//  Created by Aldo on 04/09/18.
+//  Copyright © 2018 Aldo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
